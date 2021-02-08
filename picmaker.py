bd5ad8b19e0108475f697ccdbefbdbc7d1a86541
Yulin Zheng <yulin@Yulins-MacBook-Pro.local>
2021-02-08 15:36:55 -0500

    found weird conversion issue

--- picmaker.py
+++ picmaker.py
@@ -1,7 +1,7 @@
 img = open("image.ppm", "w")
 img.write("P3 500 500 255 ")
 
-#if you view this as .ppm, you can get a different result than when you convert to .png or even .tiff (which theoretically should be lossless)
+#if you view this as .ppm, you can get a different result than when you convert to .png or even .tiff (which theoretically should not even be compressed)
 for x in range(0, 500):
     for y in range(0, 500):
         a = (abs(x - 256))
