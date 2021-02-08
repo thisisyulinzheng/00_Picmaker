92670a7557c8ece73d8f240756eaa088b552ab2f
Yulin Zheng <yulin@Yulins-MacBook-Pro.local>
2021-02-08 15:58:35 -0500

    fixed conversion bug

--- picmaker.py
+++ picmaker.py
@@ -1,12 +1,11 @@
 img = open("image.ppm", "w")
 img.write("P3 500 500 255 ")
 
-#if you view this as .ppm, you can get a different result than when you convert to .png or even .tiff (which theoretically should not even be compressed)
 for x in range(0, 500):
     for y in range(0, 500):
-        a = (abs(x - 256))
-        b = (abs(y - 256))
-        c = (abs(x * y - 256))
+        a = (abs(x - 256)) % 256
+        b = (abs(y - 256)) % 256
+        c = (abs(x * y - 256)) % 256
         final_str = "{} {} {} ".format(a, b, c)
         img.write(final_str)
         
